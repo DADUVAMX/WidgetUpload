2a71eec6ebcfe334bb69eadf3eae989b901726fc
DADUVAMX <89940120+DADUVAMX@users.noreply.github.com>
2021-11-07 16:01:07 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import UploadWidget from "./UploadWidget";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <UploadWidget />
+    </div>
+  );
+}
